2716750b35f57b3b4e054fef3c08fae128eda356
yakisuzu <yakisuzu@gmail.com>
2019-06-06 14:33:05 +0900

    コマンド整形まわり

--- src/main/scala/OjichatService.scala
+++ src/main/scala/OjichatService.scala
@@ -3,10 +3,13 @@ import cats.effect.IO
 import scala.sys.process.Process
 
 class OjichatService() {
+  def makeCommand(args: String*): Seq[String] =
+    Seq("ojichat") ++ args
+
   def getTalk(name: Option[String]): IO[String] = IO {
     name match {
-      case Some(n) => Process(s"ojichat '$n'").!!
-      case _ => Process(s"ojichat").!!
+      case Some(n) => Process(makeCommand(n)) !!
+      case _ => Process(makeCommand()) !!
     }
   }
 }
