87e47aab92b88fbfdc3985ae5114dbeb023b1fe6
yakisuzu <yakisuzu@gmail.com>
2019-06-11 22:40:44 +0900

    scalatest

---
+++ src/test/scala/jp/ojisan/OjisanServiceSpec.scala
@@ -0,0 +1,14 @@
+package jp.ojisan
+
+import org.scalatest.{BeforeAndAfterEach, FunSpec}
+
+class OjisanServiceSpec extends FunSpec with BeforeAndAfterEach {
+  override def beforeEach() {}
+  override def afterEach() {}
+
+  describe("OjisanServiceSpec") {
+    it("should mentionedMessage") {}
+    it("should kimagureReaction") {}
+    it("should debugMessage") {}
+  }
+}
