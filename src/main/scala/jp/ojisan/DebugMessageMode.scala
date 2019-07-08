01539a4c64e35b9d03356db6f64345a6937ff15a
yakisuzu <yakisuzu@gmail.com>
2019-07-08 12:06:59 +0900

    debug mode

---
+++ src/main/scala/jp/ojisan/DebugMessageMode.scala
@@ -0,0 +1,34 @@
+package jp.ojisan
+
+import cats.effect.IO
+import com.typesafe.scalalogging.LazyLogging
+
+sealed abstract class DebugMessageMode(val printMessage: Boolean) extends LazyLogging {
+  def debugMessage(ojisanRepository: OjisanRepository): IO[Unit] =
+    if (printMessage) ojisanRepository.onMessage(debug)
+    else IO.unit
+
+  def debug(m: MessageValue): IO[Unit] = IO {
+    logger.debug(
+      Map(
+        "ts"                 -> m.timestamp,
+        "channelId"          -> m.channel.getId,
+        "channelName"        -> m.channel.getName,
+        "senderUserId"       -> m.sender.id,
+        "senderUserRealName" -> m.sender.realName,
+        "content"            -> m.content
+      ).toString()
+    )
+  }
+}
+
+object DebugMessageMode {
+  def apply(mode: String): DebugMessageMode = mode match {
+    case m: String if m == "ON" => new DebugMessageModeOn
+    case _                      => new DebugMessageModeOff
+  }
+
+}
+
+case class DebugMessageModeOn()  extends DebugMessageMode(true)
+case class DebugMessageModeOff() extends DebugMessageMode(false)
